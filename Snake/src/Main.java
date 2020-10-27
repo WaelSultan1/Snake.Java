d15f8cd47e1bc58a430e355ebd267b4268a52588
Wael Sultan <wael.sultan@fdmgroup.com>
2020-10-27 16:40:03 -0300

    snake project

---
+++ Snake/src/Main.java
@@ -0,0 +1,20 @@
+import javax.swing.JFrame;
+public class Main {
+
+	public Main() 
+	{
+		JFrame frame = new JFrame();
+		GamePanel gamePanel = new GamePanel();
+		frame.add(gamePanel);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setTitle("SnakeCoding");
+		frame.pack();
+		frame.setVisible(true);
+		frame.setLocationRelativeTo(null);
+	}
+	public static void main(String[] args) {
+
+		new Main();
+	}
+
+}
